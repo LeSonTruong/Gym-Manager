a068672904e188e57aa42ed53aaf93e90cd82928
LeSonTruong <LeSonTruong@icloud.com>
2026-04-02 15:17:24 +0700

    chore(test): migrate jest to swc and patch next

--- apps/nestjs-backend/jest.config.js
+++ apps/nestjs-backend/jest.config.js
@@ -3,7 +3,26 @@ module.exports = {
   rootDir: '.',
   testRegex: String.raw`.*\.spec\.ts$`,
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(t|j)s$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            decorators: true,
+          },
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+          },
+          target: 'es2022',
+        },
+        module: {
+          type: 'commonjs',
+          noInterop: true,
+        },
+      },
+    ],
   },
   collectCoverageFrom: ['src/**/*.(t|j)s'],
   coverageDirectory: './coverage',
